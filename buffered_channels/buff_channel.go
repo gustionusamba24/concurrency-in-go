8ca1b6f6179712aa18feb7dc0c18af70dadbe56b
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-09-18 15:37:30 +0700

    feat(buffered-channel): use a buffered channel for email sending

---
+++ buffered_channels/buff_channel.go
@@ -0,0 +1,11 @@
+package buffered_channels
+
+func addEmailsToQueue(emails []string) chan string {
+	ch := make(chan string, len(emails))
+
+	for _, email := range emails {
+		ch <- email
+	}
+
+	return ch
+}
---
+++ buffered_channels/buff_channel_test.go
@@ -0,0 +1,97 @@
+package buffered_channels
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gustionusamba24/concurrency-in-go/internal/format"
+)
+
+func TestAddEmailsToQueue(t *testing.T) {
+	type testCase struct {
+		emails   []string
+		expected int
+	}
+
+	runCases := []testCase{
+		{
+			[]string{
+				"To boldly go where no man has gone before.",
+				"Live long and prosper.",
+			},
+			2,
+		},
+		{
+			[]string{
+				"The needs of the many outweigh the needs of the few, or the one.",
+				"Change is the essential process of all existence.",
+				"Resistance is futile.",
+			},
+			3,
+		},
+		{
+			[]string{
+				"It's life, Jim, but not as we know it.",
+				"Infinite diversity in infinite combinations.",
+				"Make it so.",
+				"Engage!",
+			},
+			4,
+		},
+	}
+
+	testCases := runCases
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range testCases {
+		ch := addEmailsToQueue(test.emails)
+		actual := len(ch)
+		if actual != test.expected {
+			failCount++
+			t.Errorf(`---------------------------------
+Test Failed:
+  emails:
+%v
+  expected channel length: %v
+  actual channel length:   %v
+`,
+				sliceWithBullets(test.emails),
+				test.expected,
+				actual)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Test Passed:
+  emails:
+%v
+  expected channel length: %v
+  actual channel length:   %v
+`,
+				sliceWithBullets(test.emails),
+				test.expected,
+				actual)
+		}
+	}
+
+	format.Summary(len(testCases), passCount, failCount)
+}
+
+func sliceWithBullets[T any](slice []T) string {
+	if slice == nil {
+		return "  <nil>"
+	}
+	if len(slice) == 0 {
+		return "  []"
+	}
+	output := ""
+	for i, item := range slice {
+		form := "  - %#v\n"
+		if i == (len(slice) - 1) {
+			form = "  - %#v"
+		}
+		output += fmt.Sprintf(form, item)
+	}
+	return output
+}
