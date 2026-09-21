1605cd57645806dc96a87dc8595e98a1e5479d51
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-09-21 11:53:03 +0700

    refactor to confinement

---
+++ confinement_concurrency/confinement.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func process(data int) int {
+	time.Sleep(time.Second * 2)
+	return data * 2
+}
+
+func processData(wg *sync.WaitGroup, resultDest *int, data int) {
+	defer wg.Done()
+
+	processedData := process(data)
+
+	*resultDest = processedData
+}
+
+func main() {
+	start := time.Now()
+	var wg sync.WaitGroup
+
+	input := []int{1, 2, 3, 4, 5}
+	result := make([]int, len(input))
+
+	for i, data := range input {
+		wg.Add(1)
+		go processData(&wg, &result[i], data)
+	}
+
+	wg.Wait()
+
+	fmt.Println(time.Since(start))
+	fmt.Println(result)
+}
